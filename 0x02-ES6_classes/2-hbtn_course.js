dfb4da55d40b5d1fc0e62f43ec2a0c243e38705f
oladele.david <tdboy59@gmail.com>
2024-07-25 20:35:37 +0100

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+    /**
+     * Initializes a new instance of the HolbertonCourse class.
+     *
+     * @param {string} name - The name of the course.
+     * @param {number} length - The length of the course in hours.
+     * @param {number} students - The number of students enrolled in the course.
+     */
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(length) {
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = length;
+    }
+
+
+    get students() {
+        return this._students;
+    }
+
+    set students(students) {
+        if (!(students instanceof Array)) {
+            throw new TypeError('Students must be an array');
+        }
+        if (!students.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = students;
+    }
+}
