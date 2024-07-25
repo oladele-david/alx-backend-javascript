71157b1e33c91a0478b42196bb068ab9b641783c
oladele.david <tdboy59@gmail.com>
2024-07-25 19:30:17 +0100

    fix Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,14 @@
+/* eslint-disable */
+/**
+ * Represents a class room
+ */
 export default class ClassRoom {
+    /**
+     * Creates a new class room
+     * @param {number} maxStudentsSize
+     * @returns {ClassRoom}
+     * @memberof ClassRoom
+     */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
     }
