18f52421227fa96c146210e92e73344b79f2ebf2
oladele.david <tdboy59@gmail.com>
2024-07-25 03:10:10 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto } from "./utils";
+import { createUser } from "./utils";
+
+export default function handleProfileSignup() {
+    return Promise.all([createUser(), uploadPhoto()])
+    .then((results) => {
+        console.log(`${results[0].body} ${results[1].firstname} ${results[1].lastname}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
