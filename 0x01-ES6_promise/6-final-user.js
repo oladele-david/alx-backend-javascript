70e8ec815b7226d72e6a13bdc04db3ff549f6718
oladele.david <tdboy59@gmail.com>
2024-07-25 03:56:26 +0100

    made func async

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,11 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-    return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => {
-        results.map((res) => ({
-            status: res.status,
-            value: res.status === 'fulfilled' ? res.value : String(res.reason),
-        }))
-    });
+export default async function handleProfileSignup(firstName, lastName, filename) {
+    const results = await Promise
+        .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+    results.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+    }));
 }
