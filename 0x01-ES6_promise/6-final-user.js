2c2c2d860efe01b5d5c5db252a1c0419b512b756
oladele.david <tdboy59@gmail.com>
2024-07-25 04:03:42 +0100

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,18 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-    const results = await Promise
-        .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
-    results.map((res) => ({
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : String(res.reason),
-    }));
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  filename
+) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === "fulfilled" ? o.value : String(o.reason),
+    }))
+  );
 }
